67d38dc23dcf5efef41b6e8ffa1140cfb1d8267b
Giorgi <107646105+GiorgiZanqaidze@users.noreply.github.com>
2024-03-19 12:54:32 +0000

    insert item beginning of the list

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,9 @@
 
 
-import node_lists.Node;
 import node_list_upgraded.NodeListUpgraded;
 
 public class Main {
     public static void main(String[] args) {
-        // int[] array = {1, 2, 3, 5};
-        // Node head =  null;
-        // Node tail = null;
-
-        // for (int i = 0; i < array.length; i++) {
-        //     Node newNode = new Node(array[i]); // Create a new node for the current element
-
-        //     if (head == null) { // If the NodeList is empty
-        //         head = newNode; // Set the new node as the head
-        //         tail = newNode; // Also set it as the tail
-        //     } else { // If NodeList is not empty
-        //         tail.next = newNode; // Link the new node to the end of the list
-        //         tail = newNode; // Update the tail to be the new node
-        //     }
-        // }
-
-        // Node current = head;
-
-        // while(current != null) {
-        //     System.out.print(current.getData() + " ");
-        //     current = current.next;
-        // }
-
 
         int[] array = {1, 2, 3, 5};
         NodeListUpgraded head = null;
@@ -46,11 +22,23 @@ public class Main {
             }
         }
 
-        while (head != null) {
-            System.out.print(head.getData() + " ");
-            head = head.next;
+        NodeListUpgraded newHead = addNewHead(head, 213213);
+
+        printList(newHead);
+    }
+
+    private static void printList(NodeListUpgraded nodeHead) {
+
+        while (nodeHead != null) {
+            System.out.print(nodeHead.getData() + " ");
+            nodeHead = nodeHead.next;
         }
+    }
 
 
+    private static NodeListUpgraded addNewHead(NodeListUpgraded head, Object data) {
+        NodeListUpgraded newHead = new NodeListUpgraded(data);
+        newHead.next = head;
+        return newHead;
     }
 }
