4bf43eae8043b15fa838b4f07c02dd980b411d52
Giorgi <107646105+GiorgiZanqaidze@users.noreply.github.com>
2024-03-25 11:11:05 +0000

    comment

--- src/leetcode_algorithms/AddTwoNumbers.java
+++ src/leetcode_algorithms/AddTwoNumbers.java
@@ -1,6 +1,5 @@
 package leetcode_algorithms;
 
-
 public class AddTwoNumbers {
     
     public class ListNode {
@@ -11,52 +10,57 @@ public class AddTwoNumbers {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int number_1 = 0;
-        int number_2 = 0;
+        // Initialize Dummi head
+        ListNode dummyHead = new ListNode();
+        ListNode current = dummyHead;
+        
+        // For the first iterate carry must be 0
+        int carry = 0;
 
-        while (l1 != null) {
-            number_1 += l1.val; 
-            l1 = l1.next;
+        while (l1 != null || l2 != null) {
+            // Get the digit from l1 and l2
+            // If not exist get 0
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            // Add digits with carry
+            int sum = x + y + carry;
+            // Remember the new carry value
+            carry = sum / 10;
+
+            // Save new node to the next
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
 
-        while (l2 != null) {
-            number_2 += l2.val; 
-            l2 = l2.next;
+        if (carry > 0) {
+            current.next = new ListNode(carry);
         }
 
-        String numberStr_1 = String.valueOf(number_1);
-        String numberStr_1_reversed = "";
-
-        for (int i = numberStr_1.length(); i > 0; i--) {
-            numberStr_1_reversed += numberStr_1.toCharArray()[i];
-        }
-
-        String numberStr_2 = String.valueOf(number_2);
-        String numberStr_2_reversed = "";
-
-        for (int i = numberStr_1.length(); i > 0; i--) {
-            numberStr_2_reversed += numberStr_2.toCharArray()[i];
-        }
-
-        int sumOfNums = Integer.parseInt(numberStr_1_reversed) + Integer.parseInt(numberStr_2_reversed);
-
-
-        ListNode result = new ListNode(String.valueOf(sumOfNums).toCharArray()[0]);
-
-        for (int i = 0; i < String.valueOf(sumOfNums).length(); i++) {
-            result.next = new ListNode(String.valueOf(sumOfNums).toCharArray()[i]);
-            result = result.next;
-        }
-
-        return result;
+        return dummyHead.next;
     }
-
 
     public static void main(String[] args) {
-       
-    }
+        // Example usage
+        AddTwoNumbers solution = new AddTwoNumbers();
+        AddTwoNumbers.ListNode l1 = solution.new ListNode(2);
+        l1.next = solution.new ListNode(4);
+        l1.next.next = solution.new ListNode(3);
 
-    
+        AddTwoNumbers.ListNode l2 = solution.new ListNode(5);
+        l2.next = solution.new ListNode(6);
+        l2.next.next = solution.new ListNode(4);
+
+        AddTwoNumbers.ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Printing the result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
 }
